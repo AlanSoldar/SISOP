7c53757646ee47b5519ec120abeea5778ad1e1bd
Gabriel Martins <gabrielmartins173@gmail.com>
2022-03-15 21:04:34 -0300

    creating and setting info for Defines and Packet

---
+++ Defines.hpp
@@ -0,0 +1,48 @@
+#ifndef PACKET_TYPES
+#define PACKET_TYPES
+enum{
+    NOTIFICATION_PKT = 100,     // Notification sent by someone the user is following
+    MESSAGE_PKT,                // To communicate command errors and similar stuff 
+    COMMAND_FOLLOW_PKT,         // User wants to follow someone
+    COMMAND_SEND_PKT,           // User wants to send a notification
+    USER_INFO_PKT,              // Sends a username in the payload
+    SESSION_OPEN_SUCCEDED,      // When server could connect client to a session
+    SESSION_OPEN_FAILED         // When server could not connect client to a session
+};
+#endif
+
+#ifndef PORT
+#define PORT 4000
+#endif
+
+#ifndef MAX_PAYLOAD_LENGTH
+#define MAX_PAYLOAD_LENGTH 256
+#endif
+
+#ifndef MAX_AUTHOR_LENGTH
+#define MAX_AUTHOR_LENGTH 18
+#endif
+
+#ifndef MAX_NOTIFICATION_LENGTH
+#define MAX_NOTIFICATION_LENGTH 128
+#endif
+
+#ifndef PKT_HEADER_BUFFER_LENGTH
+#define PKT_HEADER_BUFFER_LENGTH 4     
+#endif
+
+#ifndef CR
+#define CR 13
+#endif 
+
+#ifndef LF
+#define LF 10
+#endif 
+
+#ifndef SERVER_ADDR
+#define SERVER_ADDR "127.0.0.1"
+#endif
+
+#ifndef MAX_TCP_CONNECTIONS
+#define MAX_TCP_CONNECTIONS 256
+#endif
---
+++ Packet.hpp
@@ -0,0 +1,42 @@
+#ifndef PACKET_HEADER
+#define PACKET_HEADER
+
+#include <iostream>
+#include <stdint.h>
+#include <string.h>
+#include <string>
+#include "Defines.hpp"
+
+
+class Packet {
+
+    private:
+        uint16_t type;      // See possible types in defines.hpp
+        uint16_t seqn;      // Sequence number
+        uint16_t length;    // Payload length
+        time_t timestamp;   // Data timestamp
+        char author[MAX_AUTHOR_LENGTH];        // If there is one
+        char payload[MAX_PAYLOAD_LENGTH];      // Content of the packet
+
+	public:
+        Packet();
+        Packet(uint16_t type, char const *payload);   // Get the timestamp when initializing the object
+        Packet(uint16_t type, time_t timestamp, char const *payload);   
+        Packet(uint16_t type, time_t timestamp, char const *payload, char const *author); // If it's a notification
+
+		uint16_t getType();
+		uint16_t getSeqn();
+		uint16_t getLength();
+		time_t getTimestamp();
+        char* getPayload();
+        char* getAuthor();
+
+        void setType(uint16_t type);
+        void setSeqn(uint16_t seqn);
+        void setTimestamp(time_t timestamp);
+        void setPayload(char* payload);
+        void setAuthor(char* author);
+};
+
+
+#endif
